4edd0c0027294e945e4548c6f0a67055498c2ea0
Twyz <baobao1013068846@sina.cn>
2021-02-21 22:45:34 +0800

    Add: Longest-1 in 0/1 string.
    [Note]
    - Correspond to Long_Substr_After_Replace.c.
    
    Signed-off-by: Twyz <baobao1013068846@sina.cn>

---
+++ LeetCode/String/Longest-One.c
@@ -0,0 +1,88 @@
+/*
+给定一个由若干 0 和 1 组成的数组 A，我们最多可以将 K 个值从 0 变成 1 。
+返回仅包含 1 的最长（连续）子数组的长度。
+
+E.g.A = [1,1,1,0,0,0,1,1,1,1,0], K = 2
+	---> 6
+		[1,1,1,0,0,1,1,1,1,1,1]
+
+	A = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], K = 3
+	---> 10
+		[0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1]
+
+[URL]
+https://leetcode-cn.com/problems/max-consecutive-ones-iii/
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TEST 1
+
+int longestOnes(int *A, int ASize, int K)
+{
+	int i = 0, j = 0, mark = 0, result = 0;
+
+	// if K >= Asize ==> return Asize
+	if(K >= ASize)
+		return ASize;
+
+	while(j < ASize){
+		if(A[j] == 0)
+			mark++;
+#if TEST
+		printf("[%d]-[%d]  len=%d mark=%d\n", i, j, j-i+1, mark);
+		for(int s = i; s <= j; s++)
+			printf("%d ", A[s]);
+		printf("\n");
+#endif
+		if(mark > K){
+			result = (j-i > result)? j-i : result;
+			// head move on
+			while(i <= j){
+				if(A[i++] == 0){
+					if(K > 0){
+						mark--;
+						break;
+					}else{
+						mark = 0;
+					}
+				}
+			}
+		}else{
+			result = (j-i+1 > result)? j-i+1 : result;
+		}
+		// tail move on
+		j++;
+	}
+
+	return result;
+}
+
+int main()
+{
+	int a1[] = {1,1,1,0,0,0,1,1,1,1};
+	int a2[] = {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
+	int a3[] = {0,0,0,1};
+	int K = 0;
+	int *x = a1;
+	int length = 0, ret = 0;
+
+	/*
+	  注意：C语言不能通过指针获取所指向的内存大小 !!!
+	  解决：从数组获取 (唯一办法)
+	*/
+	length = sizeof(a1)/sizeof(int);
+	//print
+#if TEST
+	printf("len = %d\nA = [", length);
+#endif
+	for(int i = 0; i < length; i++){
+		printf("%d ", x[i]);
+	}
+	printf("]\tK = %d\n", K);
+	
+	ret = longestOnes(x, length, K);
+	printf("==> %d\n", ret);
+}
+
