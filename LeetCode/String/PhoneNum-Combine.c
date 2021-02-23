c46dc75bdd70667ca3caf7cce33ff54ebdae6a79
Twyz <baobao1013068846@sina.cn>
2021-02-23 14:57:52 +0800

    Modify: Combinations of chars in Phone Numbers
    [Note]
    - Learn 'backtrace' to apply on other situations.
    - 'Backtrace' is similar to DFS.
    
    Signed-off-by: Twyz <baobao1013068846@sina.cn>

--- LeetCode/String/PhoneNum-Combine.c
+++ LeetCode/String/PhoneNum-Combine.c
@@ -20,10 +20,13 @@ https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
 #include <string.h>
 
 #define TEST 0
+#define USE_MY_CODE 0		// My method is unhandy.
 
+static char phone[8][5] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+#if USE_MY_CODE
 char **letterCombinations(char *digits, int returnSize)
 {
-	char phone[8][5] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 	char *p = digits, *q = NULL;
 	char tmp[2] = {0};
 	char **comb = NULL;
@@ -67,37 +70,90 @@ char **letterCombinations(char *digits, int returnSize)
 		}
 		p++;
 	}
-
 	return comb;
-	
 }
+#else
+/*
+回朔法: 选优搜索法，按选优向前搜索，以达到目的。当达到某处不满足条件，退回一步。
+中心思想 ----> DFS
+[使用场景]
+	1. 排列组合问题;
+
+*/
+char *cmb_tmp = NULL;		// Temp store cmb-str. [Important!!!]
+int index_tmp = 0;			// Index of tmp.
+int digit_size = 0; 		// length of digits
+int cmb_size = 0;			// record size of cmb.
+
+void backtrace_phone(char **cmb, int index, char *digits)
+{
+	int i = 0, n = 0;
+#if TEST
+	printf("idx_tmp=%d cmb_tmp:%s\n", index_tmp, cmb_tmp);
+#endif
+	if(index == digit_size){
+		char *x = (char *)malloc(sizeof(char) * (digit_size + 1));
+		memcpy(x, cmb_tmp, sizeof(char) * (digit_size + 1));
+		cmb[cmb_size++] = x;
+	}else{
+		n = digits[index] - '2';
+		for(i = 0; i < strlen(phone[n]); i++){
+			cmb_tmp[index_tmp++] = phone[n][i];
+			backtrace_phone(cmb, index+1, digits);
+			cmb_tmp[index_tmp--] = '\0';
+		}
+	}
+}
+
+char **letterCombinations(char *digits, int *returnSize)
+{
+	char **cmb;
+	int i = 0, len = 1;
+
+	digit_size = strlen(digits);
+	if(digit_size == 0)
+		return cmb;
+
+	for(i = 0; i < digit_size; i++){
+		if(digits[i] == '7' || digits[i] == '9')
+			len *= 4;
+		else
+			len *= 3;
+	}
+	cmb = (char **)malloc(sizeof(char*) * len);
+	cmb_tmp = (char *)malloc(sizeof(char) * (digit_size + 1));
+
+	backtrace_phone(cmb, 0, digits);
+	*returnSize = cmb_size;
+	return cmb;
+}
+#endif
 
 void Print_Letters(int num)
 {
 	char s[8] = {0};
 	char **ret;
-	int i = 0;
-	int size = 1;
+	int i = 0, size = 1;
 
+	printf("[Num] %d\n", num);
 	snprintf(s, sizeof(s), "%d", num);
-
+#if USE_MY_CODE
 	for(i = 0; i < strlen(s); i++){
 		if(s[i] == '7' || s[i] == '9')
 			size *= 4;
 		else
 			size *= 3;
 	}
-
-	printf("[Num] %d size=%d\n", num, size);
 	ret = letterCombinations(s, size);
-	// print
-	printf("==> [ ");
-	for(i = 0; i < size && ret[i]; i++){
+#else
+	ret = letterCombinations(s, &size);
+#endif
+	printf("size=%d\n==> [ ", size);
+	for(i = 0; i < size; i++){
 		printf("%s ", ret[i]);
 		free(ret[i]);
 	}
 	printf("]\n");
-
 	//free
 	free(ret);
 }
@@ -108,7 +164,7 @@ int main()
 	int n2 = 49;
 	int n3 = 294;
 	// sample here
-	int x = n3;
+	int x = n2;
 
 	Print_Letters(x);
 }
