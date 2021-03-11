b141a64e8f9cc817241d71a98d1d41a9bbfb5050
Twyz <baobao1013068846@sina.cn>
2021-03-11 11:19:05 +0800

    Modify: md5_check.c

--- C/md5_test/md5_check.c
+++ C/md5_test/md5_check.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-#define CHECK_FILE "./LX"
+#define TEST_FILE "./LX"
 #define MD5_FILE "./LX.md5"
+#define TMP_FILE "./MD5.txt"
 
 void *strupr(char *str)
 {
-	for(;*str != '\0'; *str++)
-	{
+	for(;*str != '\0'; *str++){
 		if(*str >= 97 && *str <= 122)
 			*str = (*str)-32;
 	}
@@ -16,19 +17,17 @@ void *strupr(char *str)
 
 char *cat_file(char *name)
 {
-	int i;
-	FILE *fp;
-	static char buf[512];
+	int i = 0;
+	FILE *fp = NULL;
+	static char buf[512] = {0};
 
-	buf[0] = '\0';
-
-	fp = fopen(name, "r");
-	if (fp == NULL)
+	if((fp = fopen(name, "r")) == NULL)
 		return buf;
-	fgets(buf, sizeof(buf), fp);
+
+	if(fgets(buf, sizeof(buf), fp) == NULL)
+		printf("Read failed from %s!!\n", name);
 	fclose(fp);
 
-	i = 0;
 	while (buf[i] != '\0' && buf[i] != '\r' && buf[i] != '\n')
 		i++;
 	buf[i] = '\0';
@@ -36,44 +35,44 @@ char *cat_file(char *name)
 	return buf;
 }
 
-int check_md5(char *fileName, char *md5file)
+void check_md5(char *test_file, char *md5_file)
 {
-	char cmd[128],*value;
-	char controlMd5[33],fileMd5[33];
+	char *value;
+	char cmd[128], md5_sum[64], md5_from_file[64];
 
 	/* get md5-values from md5 file */
-	value=cat_file(md5file);
-	strncpy(controlMd5,value,32);
-	controlMd5[32]='\0';
+	value = cat_file(md5_file);
+	strncpy(md5_from_file, value, 32);
+	md5_from_file[32]='\0';
 
-	/* get file's md5 */
-	sprintf(cmd,"md5sum %s > md5",fileName);
-	/* To uppercase: md5sum %s | awk '{print $1}' | tar a-z A-Z > md5 */
-	system(cmd);
+	/* md5sum get value */
+	sprintf(cmd,"md5sum %s  > %s", test_file, TMP_FILE);
+	/* Uppercase: md5sum %s | awk '{print $1}' | tar a-z A-Z  */
 
-	value=cat_file("md5");
-	strncpy(fileMd5,value,32);
-	fileMd5[32]='\0';
-	unlink("md5");	// It will rm md5 after killing the process.
+	if(system(cmd) == -1)
+		return;
 
-	/* Change fileMd5 to uppercase */
-	strupr(fileMd5);
+	value = cat_file(TMP_FILE);
+	strncpy(md5_sum, value, 32);
+	md5_sum[32]='\0';
+	unlink(TMP_FILE);	// It will rm md5 after killing the process.
 
-	printf("get md5 values from %s:\n%s\n\n", MD5_FILE, controlMd5);
-	printf("md5sum %s:\n%s\n\n", CHECK_FILE, fileMd5);
+	/* Uppercase */
+	strupr(md5_sum);
+	strupr(md5_from_file);
 
-	if( strncmp(controlMd5,fileMd5,32) == 0 )
-		return 1;
+	printf("MD5 values from %s:\n%s\n", md5_file, md5_from_file);
+	printf("MD5 values from md5sum:\n%s\n\n", md5_sum);
+
+	if(!strncmp(md5_sum, md5_from_file,32))
+		printf("The same md5 --> The file has not been changed.\n");
 	else
-		return 0;
+		printf("Different md5 --> The file has been changed.\n");	
 }
 
 
 int main()
 {
-	if(check_md5(CHECK_FILE, MD5_FILE) == 1)
-		printf("The same md5 --> The file has not been changed.\n");
-	else
-		printf("Different md5 --> The file has been changed.\n");	
+	check_md5(TEST_FILE, MD5_FILE);
 }
 
